1f5fd7503bc6201cc77e73934c63e134ccff9760
Waqas Gulzar <zaviarxav@outlook.com>
2018-09-11 10:42:29 +0500

    media library services

--- src/app/shared/services/mediaLibrary.service.ts
+++ src/app/shared/services/mediaLibrary.service.ts
@@ -4,8 +4,6 @@ import * as _model from '../../shared/models/models';
 import { HttpClient } from '@angular/common/http';
 import { ErrorHandlerService } from '../../shared/services/error-handler.service';
 import { Entity } from '../../shared/models/entity';
-import { environment } from '../../../environments/environment';
-import { catchError, map } from 'rxjs/operators';
 
 @Injectable()
 export class MediaLibraryService extends RestService<_model.MediaLibrary> {
@@ -26,19 +24,5 @@ export class MediaLibraryService extends RestService<_model.MediaLibrary> {
 
   getInstance(): Entity {
     return new _model.MediaLibrary();
-  }
-
-  upload(formData: FormData) {
-    return this.http.post<Entity>(environment.apiUrl + this.getUri(), formData).pipe(
-      catchError(this.errorHandler.handleError),
-      map((response: Entity) => {
-        return this.getInstance().deserialize(response);
-      })
-    );
-  }
-
-  private handleError(error: Response | any) {
-    console.error('File Uploading Error: ', error);
-    return error;
   }
 }
